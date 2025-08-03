2cb7a61d9e3a11792121c26500920e2f61efc42d
Dušan Rosić (SRB) <149257819+dusanrsc@users.noreply.github.com>
2025-08-03 21:51:08 +0200

    Update MainActivity.kt

--- app/src/main/java/com/infinitysoftware/simplenavigation/MainActivity.kt
+++ app/src/main/java/com/infinitysoftware/simplenavigation/MainActivity.kt
@@ -16,7 +16,6 @@ class MainActivity : ComponentActivity() {
         setContent {
             SimpleNavigationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
                     Navigation()
                 }
             }
